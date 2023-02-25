7a35f1c4399263e3de853b6dcfdbc5476f5ad545
Roj <ez@roj.im>
2023-02-25 11:56:46 +0300

    a

--- src/nuveke.nim
+++ src/nuveke.nim
@@ -1,11 +1,16 @@
-import std/tables
-import std/json
-import httpbeast
-import handler
+# import std/tables
+# import std/json
+# import httpbeast
+# import handler
+import std/osproc
 
 when isMainModule:
-  let jsonNode = parseFile("/etc/nuveke.json")
-  var config = initTable[string, string]()
-  for key in jsonNode.keys:
-    config[key] = jsonNode[key].getStr()
-  run(getHandler(config))
+  let output = execCmd("echo 1")
+
+  echo output
+
+  # let jsonNode = parseFile("/etc/nuveke.json")
+  # var config = initTable[string, string]()
+  # for key in jsonNode.keys:
+  #   config[key] = jsonNode[key].getStr()
+  # run(getHandler(config))
